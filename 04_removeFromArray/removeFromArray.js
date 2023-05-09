ce358d11334c241be1c8f9c05f44d4df12c59eef
Crate8334 <130948804+Crate8334@users.noreply.github.com>
2023-05-09 12:05:57 +0800

    finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    // for (const e of args) {
+    //     while (array.indexOf(e)>-1) {
+    //         array.splice(array.indexOf(e), 1);
+    //     }
+    // }
 
+    return array.filter(
+        (element) => !args.includes(element)
+    );
 };
 
 // Do not edit below this line
