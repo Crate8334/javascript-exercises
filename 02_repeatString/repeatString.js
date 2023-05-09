343226bff8e725fc29f7772d14882f2a0f2e5ab4
Crate8334 <130948804+Crate8334@users.noreply.github.com>
2023-05-09 10:42:45 +0800

    finish exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+    let result = '';
+    if (times < 0) return 'ERROR';
+    for (let i = 0; i < times; i++) {
+        // result = result.concat(string);
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
