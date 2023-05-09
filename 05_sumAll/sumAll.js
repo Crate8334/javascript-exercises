2833418b398c7aabc528a3b680c97e5f74d414a5
Crate8334 <130948804+Crate8334@users.noreply.github.com>
2023-05-09 14:19:02 +0800

    finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (!(Number.isInteger(num1) && Number.isInteger(num2))) {
+        return "ERROR";
+    }
+    let startNum = num1 < num2 ? num1 : num2;
+    let endNum = num1 > num2 ? num1 : num2;
+    if (startNum < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
