93cc9f2b9bf049b3cf2099d28ba325a41598f1c6
Crate8334 <130948804+Crate8334@users.noreply.github.com>
2023-05-09 10:59:24 +0800

    finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const stringArray = string.split('');
+    const reversedStringArray = stringArray.reverse();
+    const result = reversedStringArray.join('');
+    return result;
 };
 
 // Do not edit below this line
