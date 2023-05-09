be50192f04596a855e9d203b619b23a9deb41844
Crate8334 <130948804+Crate8334@users.noreply.github.com>
2023-05-09 14:24:29 +0800

    make some changes to exercise 5 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,8 +2,15 @@ const sumAll = function (num1, num2) {
     if (!(Number.isInteger(num1) && Number.isInteger(num2))) {
         return "ERROR";
     }
-    let startNum = num1 < num2 ? num1 : num2;
-    let endNum = num1 > num2 ? num1 : num2;
+    // let startNum = num1 < num2 ? num1 : num2;
+    // let endNum = num1 > num2 ? num1 : num2;
+    let startNum = num1;
+    let endNum = num2;
+    if (num1 > num2) {
+        startNum = num2;
+        endNum = num1;
+    }
+
     if (startNum < 0) {
         return "ERROR";
     }
